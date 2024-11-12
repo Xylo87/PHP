ca6cdd0f2c6723fc62f4404aa4371ced4d0fd2aa
Xylo87 <theo.arbogast@hotmail.fr>
2024-11-12 22:22:50 +0100

    Palindrome

--- AlgoPHP/Partie1/exo4.php
+++ AlgoPHP/Partie1/exo4.php
@@ -11,14 +11,24 @@ Ecrire un algorithme permettant de savoir si une phrase est palindrome.
 $phrase = "Engage le jeu que je le gagne";
 
 $phraseMinus = strtolower($phrase);
-$phraseReverse = strrev($phraseMinus);
-$phraseSpace = str_replace(" ", "", $phraseReverse);
-$phraseMaj = ucfirst($phraseSpace);
-$phraseFinal = substr($phraseMaj, 0, 6)." ".substr($phraseMaj, 6, 2)." ".substr($phraseMaj, 8, 3)." ".substr($phraseMaj, 11, 3)." ".substr($phraseMaj, 14, 2)." ".substr($phraseMaj, 16, 2)." ".substr($phraseMaj, 18, 5);
+$phraseSpace = str_replace(" ", "", $phraseMinus);
+$phraseReverse = strrev($phraseSpace);
 
-if ($phraseFinal === $phrase) {
-    echo "La phrase « Engage le jeu que je le gagne » est un palindrome.";
+// => Suite de l'algorithme avec la phrase de l'énoncé
+// $phraseMaj = ucfirst($phraseReverse);
+// $phraseFinal = substr($phraseMaj, 0, 6)." ".substr($phraseMaj, 6, 2)." ".substr($phraseMaj, 8, 3)." ".substr($phraseMaj, 11, 3)." ".substr($phraseMaj, 14, 2)." ".substr($phraseMaj, 16, 2)." ".substr($phraseMaj, 18, 5);
+
+if ($phraseSpace === $phraseReverse) {
+    echo "La phrase est un palindrome.";
 }
 else {
     echo "La phrase n'est pas un palindrome.";
 }
+
+// => Condition avec la phrase de l'énoncé
+// if ($phraseFinal === $phrase) {
+//     echo "La phrase « Engage le jeu que je le gagne » est un palindrome.";
+// }
+// else {
+//     echo "La phrase « Engage le jeu que je le gagne » n'est pas un palindrome.";
+// }
