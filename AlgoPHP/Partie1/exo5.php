140c6aff5e228c3ed000e50361660865f1250a91
Xylo87 <theo.arbogast@hotmail.fr>
2024-11-13 14:03:25 +0100

    exo5

---
+++ AlgoPHP/Partie1/exo5.php
@@ -0,0 +1,16 @@
+<h1>Exercice 5</h1>
+
+<p>
+Ecrire un algorithme qui déclare une valeur en francs et qui la convertit en euros.
+Attention, la valeur générée devra être arrondie à 2 décimales.
+</p>
+
+<h2>Résultat</h2>
+
+<?php
+
+$francs = 100;
+$euros = round($francs / 6.55957, 2);
+
+echo "Montant en francs : ".$francs."<br>".
+"100 francs = ".$euros."€";
