008a6d918ba59aff13a71c8726ac2f6ec97696df
Théo Arbogast <theo.arbogast@hotmail.fr>
2024-11-14 11:36:54 +0100

    exo9 + exo12 _start

---
+++ AlgoPHP/Partie1/exo12.php
@@ -0,0 +1,21 @@
+<h1>Exercice 12</h1>
+
+<p>
+A partir d’une fonction personnalisée et à partir d’un tableau de prénoms et de langue associée 
+(tableau contenant clé et valeur), dire bonjour aux différentes personnes dans leur langue 
+respective (français ➔ « Salut », anglais ➔ « Hello », espagnol ➔ « Hola ») en utilisant un switch.
+
+Exemple : tableau ➔ Mickaël => FRA, Virgile => ESP, Marie-Claire => ENG
+
+Variante : trier d’abord le tableau par ordre alphabétique du prénom
+</p>
+
+<h2>Résultat</h2>
+
+<?php
+
+$personnes = [
+    "Salut" => "Mickaël",
+    "Hola" => "Virgile",
+    "Hello" => "Marie-Claire",
+];
--- AlgoPHP/Partie1/exo9.php
+++ AlgoPHP/Partie1/exo9.php
@@ -2,7 +2,8 @@
 
 <p>
 Nous souhaitons savoir si une personne est imposable en fonction de son âge et de son sexe.
-Si la personne est une femme dont l’âge est compris entre 18 et 35 ans ou si c’est un homme de plus de 20 ans, alors celle-ci est imposable (sinon ce n’est pas le cas, « non imposable »).
+Si la personne est une femme dont l’âge est compris entre 18 et 35 ans ou si c’est un homme de plus de 20 ans, 
+alors celle-ci est imposable (sinon ce n’est pas le cas, « non imposable »).
 </p>
 
 <h2>Résultat</h2>
@@ -11,3 +12,15 @@ Si la personne est une femme dont l’âge est compris entre 18 et 35 ans ou si 
 
 $age = 32;
 $sexe = "F";
+
+echo "Age : $age<br>Sexe : $sexe <br>";
+
+if ($sexe === "H" && $age > 20) {
+    echo "La personne est imposable.";
+    } elseif ($sexe === "F" && $age >= 18 && $age <= 35) {
+    echo "La personne est imposable.";
+    }
+    else {
+        echo "Personne non imposable.";
+    }
+
